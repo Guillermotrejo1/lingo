f2f7a9f0bf2d799b5bc1e57fa6ee7d557f798124
Guillermo Trejo <maynardtrejo@yahoo.com>
2024-09-20 06:43:46 -0500

    set up the learn page showing user interaction and added loading states

--- db/queries.ts
+++ db/queries.ts
@@ -1,9 +1,29 @@
 import { cache } from "react";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+
 import db from "@/db/drizzle";
+import { userProgress } from "@/db//schema";
+
+export const getUserProgress = cache(async () => {
+    const { userId } = await auth()
+
+    if (!userId) {
+        return null
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true
+        }
+    })
+    return data
+})
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
 
   return data;
 });
-//this is so i dont hav to all props every time
+//this is so i dont have to all props every time
