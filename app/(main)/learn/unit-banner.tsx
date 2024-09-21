1799dd01b10ca6ffece0c7603398f83be33ee892
Guillermo Trejo <maynardtrejo@yahoo.com>
2024-09-21 16:36:26 -0500

    added button to unit-banner

--- app/(main)/learn/unit-banner.tsx
+++ app/(main)/learn/unit-banner.tsx
@@ -1,23 +1,31 @@
-type Props = {
-    title: string;
-    description: string;
-}
+import { Button } from "@/components/ui/button";
+import { NotebookText } from "lucide-react";
+import Link from "next/link";
 
-export const UnitBanner = ({
-    title,
-    description
-}: Props) => {
-    return (
-        <div className="w-full rounded-xl bg-green-500 p-5
-        text-white flex items-center justify-center">
-            <div className="space-y-2.5">
-                <h1>
-                    {title}
-                </h1>
-                <p>
-                    {description}
-                </p>
-            </div>
-        </div>
-    )
-}
+type Props = {
+  title: string;
+  description: string;
+};
+
+export const UnitBanner = ({ title, description }: Props) => {
+  return (
+    <div
+      className="w-full rounded-xl bg-green-500 p-5
+        text-white flex items-center justify-between"
+    >
+      <div className="space-y-2.5">
+        <h3 className="text-2xl font-bold">{title}</h3>
+        <p className="text-lg">{description}</p>
+      </div>
+      <Link href="/lesson">
+      <Button
+      size="lg"
+      variant="secondary"
+      className="hidden lg:flex"
+      >
+        <NotebookText className="mr-2" />
+      </Button>
+      </Link>
+    </div>
+  );
+};
