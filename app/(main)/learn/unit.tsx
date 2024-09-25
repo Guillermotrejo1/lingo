11a178db3f5b078fc3052a23565dcd5ad229bb53
Guillermo Trejo <maynardtrejo@yahoo.com>
2024-09-25 13:53:10 -0500

    added icons for lessons in the learn page

--- app/(main)/learn/unit.tsx
+++ app/(main)/learn/unit.tsx
@@ -33,8 +33,7 @@ export const Unit = ({
     <UnitBanner title={title} description={description}/>
     <div className="flex items-center flex-col relative">
         {lessons.map((lesson, index) => {
-            const isCurrent = true || lesson.id === activeLesson?.id;
-            //TODO: Remove hardcoded true
+            const isCurrent = lesson.id === activeLesson?.id;
             const isLocked = !lesson.completed && !isCurrent;
 
             return (
