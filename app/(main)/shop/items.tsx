5ec35534a98bb521f8788372c265c1c353ec5225
Guillermo Trejo <maynardtrejo@yahoo.com>
2024-10-05 19:53:45 -0500

    successfully added stripe

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { refillHearts } from "@/actions/user-progress";
+import { createStripeUrl } from "@/actions/user-subscription";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useTransition } from "react";
@@ -30,7 +31,19 @@ export const Items = ({
             refillHearts()
             .catch(() => toast.error("Something went wrong"))
         })
-     }
+     };
+
+     const onUpgrade = () => {
+        startTransition(() => {
+            createStripeUrl()
+            .then((response) => {
+                if (response.data) {
+                    window.location.href = response.data
+                }
+            })
+            .catch(() => toast.error("Something went wrong"))
+        });
+     };
 
   return (
     <ul className="w-full">
@@ -73,6 +86,26 @@ export const Items = ({
             )
             }
         </Button>
+        </div>
+        <div className="flex items-center w-full p-4 pt-8 gap-x-4 border-t-2">
+            <Image
+            src="/unlimited.svg"
+            alt="Unlimited"
+            height={60}
+            width={60}
+            />
+            <div className="flex-1">
+                <p className="text-neutral-700 text-base lg:text-xl
+            font-bold">
+                    Unlimited hearts
+                </p>
+            </div>
+            <Button
+            onClick={onUpgrade}
+            disabled={pending || hasActiveSubscription}
+            >
+                {hasActiveSubscription ? "active" : "upgrade"}
+            </Button>
         </div>
     </ul>
   );
--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -1,23 +1,28 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
-import { getUserProgress } from "@/db/queries";
+import { getUserProgress, getUserSubscription } from "@/db/queries";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 import { Items } from "./items";
 
 const ShopPage = async () => {
     const userProgressData = getUserProgress();
+    const userSubscriptionData = getUserSubscription();
 
     const [
-        userProgress
+        userProgress,
+        userSubscription
     ] =  await Promise.all([
-        userProgressData
+        userProgressData,
+        userSubscriptionData,
     ]);
 
     if (!userProgress || !userProgress.activeCourse) {
         redirect("/courses")
     }
+
+    const isPro = !!userSubscription?.isActive
 
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -26,7 +31,7 @@ const ShopPage = async () => {
                 activeCourse={userProgress.activeCourse}
                 hearts={userProgress.hearts}
                 points={userProgress.points}
-                hasActiveSubscription={false}
+                hasActiveSubscription={isPro}
                 />
             </StickyWrapper>
             <FeedWrapper>
@@ -47,7 +52,7 @@ const ShopPage = async () => {
                     <Items
                     hearts={userProgress.hearts}
                     points={userProgress.points}
-                    hasActiveSubscription={false} //TODO: Add subscription
+                    hasActiveSubscription={isPro} //TODO: Add subscription
                      />
                 </div>
             </FeedWrapper>
