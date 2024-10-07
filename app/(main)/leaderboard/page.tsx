e61c7900df79d5fa90bd07a8e6bb53b669791fd5
Guillermo Trejo <maynardtrejo@yahoo.com>
2024-10-06 23:00:59 -0500

    leaderboard complete

--- app/(main)/leaderboard/page.tsx
+++ app/(main)/leaderboard/page.tsx
@@ -1,5 +1,7 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
 import { UserProgress } from "@/components/user-progress";
 import { getTopUsers, getUserProgress, getUserSubscription } from "@/db/queries";
 import Image from "next/image";
@@ -51,9 +53,28 @@ const LeaderboardPage = async () => {
                     <p className="text-muted-foreground text-center text-lg mb-6">
                         See where you stand among other learners in the community.
                     </p>
+                    <Separator className="mb-4 h-0.5 rounded-full" />
                     {leaderboard.map((userProgress, index) => (
-                        <div key={userProgress.userId}>
-                            {userProgress.userName}
+                        <div 
+                        key={userProgress.userId}
+                        className="flex items-center w-full p-2 px-4 rounded-xl
+                        hover:bg-gray-200/50"
+                        >
+                            <p className="font-bold text-lime-700 mr-4">{index + 1}</p>
+                            <Avatar
+                            className="border bg-green-500 h-12 w-12 ml-3 mr-6"
+                            >
+                                <AvatarImage
+                                className="object-cover"
+                                src={userProgress.userImageSrc}
+                                />
+                            </Avatar>
+                            <p className="font-bold text-neutral-800 flex-1">
+                                {userProgress.userName}
+                            </p>
+                            <p className="text-muted-foreground">
+                                {userProgress.points} XP
+                            </p>
                         </div>
                     ))}
                 </div>
